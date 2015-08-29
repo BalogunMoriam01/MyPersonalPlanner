d0097704b97a1b37ba989057d4939f897b90264e
Balogun Moriam <balogungbeminiyi@gmail.com>
2015-08-29 13:04:32 +0200

    First commit

---
+++ My Personal Planner/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("My_Personal_Planner")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("My_Personal_Planner")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("608ae5bd-3de8-41f3-93c6-574a775c0d3f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ My Personal Planner/index.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    var myDataRef = new Firebase('https://glaring-torch-2477.firebaseio.com/');
+
+
+    myDataRef.once('value', function (dataSnapshot) {
+        dataSnapshot.forEach(function (snap) {
+            var task = snap.val();
+            $('<div>').appendTo('.border')
+            $('<div/>').text(' ').prepend($('<em/>').text(task.date)).appendTo($('.divv'))
+            $('<div/>').text(' ').prepend($('<em/>').text(task.time)).appendTo($('.divv'))
+            $('<div/><br/>').text(' ').prepend($('<em/>').text(task.task)).appendTo($('.divv'))
+        });
+    });
+
+    $('#submit').click(function () {
+        var date = $('#date').val();
+        var time = $('#time').val();
+        var task = $('#task').val();
+
+        myDataRef.push({ date: date, time: time, task: task });
+        myDataRef.limitToLast(1).on('child_added', function (snapshot) {
+            var task = snapshot.val();
+            dataSnapshot.forEach(function (snap) {
+                var task = snap.val();
+                $('<div/>').text(' ').prepend($('<em/>').text(task.date + '  ' + task.time + ' ' + task.task)).appendTo($('.divv'))
+            });
+            //       $('<div/>').text(' ').prepend($('<em/>').text(task.date + '  '+task.time + ' '+task.task)).appendTo($('.divv'))
+        });
+    });
+});
